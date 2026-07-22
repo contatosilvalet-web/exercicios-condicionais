3fcf3b64c8062b5367c55bd769ba77bcefbe00dd
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 20:59:00 -0300

    Jogada par ou impar

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,33 @@
 const jogada1 = 5;
 const jogada2 = 3;
 
-//seu código aqui
+const soma = jogada1 + jogada2;
+
+if (soma % 2 === 0) {
+  console.log("par");
+} 
+else {
+  console.log("ímpar");
+}
+
+// Imprime "par"
+
+
+
+
+/* Teste de soma de jogadas:
+- Se a soma das jogadas for par, imprime "par"
+- Se a soma das jogadas for ímpar, imprime "ímpar"
+*/
+
+/* Exemplo 01
+const jogada1 = 9;
+const jogada2 = 2;
+
+Imprime "ímpar" porque 9 + 2 = 11, que é ímpar.*/
+
+/* Exemplo 02
+const jogada1 = 2;
+const jogada2 = 4;  
+
+Imprime "par" porque 2 + 4 = 6, que é par.*/
