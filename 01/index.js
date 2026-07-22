4cf7f2622b86eadea9d4925646c24761d1d7964a
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 19:21:28 -0300

    exercício pedra, papel ou tesoura resolvido
    Added logic to determine the winner in a rock-paper-scissors game.

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,30 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "pedra";
+const jogada2 = "tesoura";
 
-//seu código aqui
+// Verificar se dá empate
+if (jogada1 === jogada2) {
+  console.log("empate");
+} 
+
+// Verficar se pedra vence
+else if (
+  (jogada1 === "pedra" && jogada2 === "tesoura") || 
+  (jogada2 === "pedra" && jogada1 === "tesoura")
+) {
+  console.log("pedra");
+} 
+
+// Verificar se papel vence
+else if (
+  (jogada1 === "papel" && jogada2 === "pedra") || 
+  (jogada2 === "papel" && jogada1 === "pedra")
+) {
+  console.log("papel");
+} 
+
+// Eliminação 
+else {
+  console.log("tesoura");
+}
+
+// Imprime "pedra"
