d4cac397d906f7117fe067ae325b31a48737eb83
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 14:16:58 -0300

    LETRA B: verificação padrão para caso o número informado não esteja no intervalo de 1 a 7.

--- 12/index.js
+++ 12/index.js
@@ -26,4 +26,42 @@ else if (diaDaSemana === 7) {
 
 // Imprime "Quarta Feira"
 
+// Imprime "Quarta Feira"
+
+
+/* Exemplos
+const diaDaSemana = 1;
+Imprime "Segunda Feira"
+
+const diaDaSemana = 7;
+Imprime "Domingo" */
+
+// LETRA B
+const diaDaSemana = 10;
+
+if (diaDaSemana === 1) {
+  console.log("Segunda Feira");
+}
+else if (diaDaSemana === 2) {
+  console.log("Terça Feira");
+}
+else if (diaDaSemana === 3) {
+  console.log("Quarta Feira");
+}   
+else if (diaDaSemana === 4) {   
+  console.log("Quinta Feira");
+}
+else if (diaDaSemana === 5) {
+  console.log("Sexta Feira");
+}   
+else if (diaDaSemana === 6) {
+  console.log("Sábado");
+}
+else if (diaDaSemana === 7) {
+    console.log("Domingo");
+} else {
+    console.log("O dia da semana informado não é válido.");
+}
+
+// Imprime "O dia da semana informado não é válido."
 
