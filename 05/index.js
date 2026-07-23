7f92baeb1fb2a90e0951c803a00626ddd43015c3
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 21:25:16 -0300

    teste de altura mínima para aprovar ou reprovar jogador

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,16 @@
 const alturaEmCm = 185;
 
-//seu código aqui
+if (alturaEmCm >= 180) {
+    console.log("APROVADO");
+} else {
+    console.log("REPROVADO");
+}
+
+// Imprime "APROVADO"
+
+/* Exemplos
+const alturaEmCm = 155;
+Imprime "REPROVADO"
+
+const alturaEmCm = 195;
+Imprime "APROVADO" */
