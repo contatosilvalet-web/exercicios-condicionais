2dc85825cd5da3ebaf23373d1a2a477e1383f3e6
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 21:31:47 -0300

    definindo as posições dos jogadores do exer 05 pela altura

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,32 @@
 const alturaEmCm = 185;
 
-//seu código aqui
+if (alturaEmCm < 180) {
+    console.log("REPROVADO");
+} 
+
+else if (alturaEmCm <= 185) {
+    console.log("LÍBERO");
+} 
+
+else if (alturaEmCm <= 195) {
+    console.log("PONTEIRO");
+} 
+
+else if (alturaEmCm <= 205) {
+    console.log("OPOSTO");
+} 
+else {
+    console.log("CENTRAL");
+}
+
+// Imprime "LÍBERO" 
+
+/* Exemplos
+const alturaEmCm = 193;
+Imprime "PONTEIRO"
+
+const alturaEmCm = 200;
+Imprime "OPOSTO"
+
+const alturaEmCm = 213;
+Imprime "CENTRAL" */
