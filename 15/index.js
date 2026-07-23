f633ea4cfc8aa38038421a4ea860950c305e931f
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 15:32:10 -0300

    programa para exibir somente nome, ou nome e sobrenome, ou somente apelido

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,29 @@ const primeiroNome = "Mario";
 const sobrenome = "";
 const apelido = "";
 
+if (apelido) {
+    console.log(apelido);
+} 
+else if (sobrenome) {
+    console.log(primeiroNome + " " + sobrenome);
+} 
+else {
+    console.log(primeiroNome);
+}
+
+// Imprime "Mario"
+
+/* Exemplos
+const primeiroNome = "Mario";
+const sobrenome = "Bros";
+Imprime "Mario Bros"
+
+const primeiroNome = "Ana";
+const sobrenome = "Julia";
+Imprime "Ana Julia"
+
+const primeiroNome = "João";
+const sobrenome = "Silva";
+const apelido = "Jão";
+Imprime "Jão" */
+
