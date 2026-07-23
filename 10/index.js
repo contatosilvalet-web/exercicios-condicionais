5c845e7ae30de84579eb3b1d186c1fb7c1c83236
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 13:56:29 -0300

    programa para identificar caracteres numa variável
    Added character classification for vowels, consonants, and numbers.

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,29 @@
 const caractere = "E";
+
+if (/[AEIOUaeiou]/.test(caractere)) {
+    if (caractere === caractere.toUpperCase()) {
+        console.log("Vogal maiúscula");
+    } else {
+        console.log("Vogal minúscula");
+    }
+} else if (/[0-9]/.test(caractere)) {
+    console.log("Número");
+} else {
+    console.log("Consoante");
+}
+
+// Imprime "Vogal maiúscula" 
+
+
+/* Exemplos
+const caractere = "a";
+Imprime "Vogal minúscula"
+
+const caractere = "3";
+Imprime "Número"
+
+const caractere = "b";
+Imprime "Consoante"
+
+const caractere = "U";
+Imprime "Vogal maiúscula" */ 
