93e75123da3b7af979a39629f10670ba7ecfa1fc
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 14:08:24 -0300

    exercício 11, a) programa que calcula o valor da parcela a ser paga pelo aluno
    Refactor payment logic to include conditions for contract duration, total amount paid, and income threshold.

--- 11/index.js
+++ 11/index.js
@@ -1,8 +1,17 @@
-//renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 300000; // 3.000,00 reais
+const mesesDecorridos = 12; // 12 meses
+const totalJaPagoPeloAluno = 1000000; // 10.000,00 reais
 
-// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
-const mesesDecorridos = 12;
-
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
-const totalJaPagoPeloAluno = 1000000;
+if (mesesDecorridos > 60) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois o tempo máximo de financiamento (60 meses) foi atingido.");
+} 
+else if (totalJaPagoPeloAluno >= 1800000) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois o curso já foi totalmente quitado.");
+} 
+else if (rendaMensalEmCentavos < 200000) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.");
+} 
+else {
+    const valorParcela = (rendaMensalEmCentavos * 18) / 10000; 
+    console.log(`O valor da parcela desse mês é R$ ${valorParcela} reais`);
+}
