175ec61c4b42321541f3804c4d8087a40922bb03
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 15:45:50 -0300

    programa para saber o Extrato de Compra Online
    Added calculations for remaining installments and their values.

--- 17/index.js
+++ 17/index.js
@@ -1,8 +1,31 @@
-//valor do produto comprado.
 const valorDoProduto = 100000;
-
-//quantidade de parcelas
 const quantidadeDoParcelamento = 10;
-
-//valor pago
 const valorPago = 300;
+
+const valorProdutoReais = valorDoProduto / 100;
+
+const valorParcela = valorProdutoReais / quantidadeDoParcelamento;
+
+const parcelasPagas = valorPago / valorParcela;
+
+const parcelasRestantes = quantidadeDoParcelamento - parcelasPagas;
+
+console.log(`Restam ${parcelasRestantes} parcelas de R$${valorParcela}`);
+
+// Imprime "Restam 7 parcelas de R$100"
+
+/* Exemplos
+const valorDoProduto = 100000;
+const quantidadeDoParcelamento = 10;
+const valorPago = 500;
+"Restam 5 parcelas de R$100"
+
+const valorDoProduto = 100000;
+const quantidadeDoParcelamento = 10;
+const valorPago = 100;
+"Restam 9 parcelas de R$100"
+
+const valorDoProduto = 120000;
+const quantidadeDoParcelamento = 12;
+const valorPago = 900;
+"Restam 3 parcelas de R$100" */
