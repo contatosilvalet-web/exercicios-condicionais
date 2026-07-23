c95c5c77c18e2f996b9e2ffb775a13f19643e1c3
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 21:15:29 -0300

    Lados das peças de dominó bucha ou não
    Added conditional check to compare ladoA and ladoB.

--- 03/index.js
+++ 03/index.js
@@ -1,4 +1,23 @@
 const ladoA = 3;
 const ladoB = 3;
 
-//seu código aqui
+if (ladoA === ladoB) {
+    console.log("SIM");
+} else {
+    console.log("NÃO");
+}
+
+// Imprime "SIM"
+
+/* Testes 
+Exemplo 01 
+- ladoA = 5;
+- ladoB = 3;
+
+Imprime "NÃO"
+
+Exemplo 02
+- ladoA = 4;
+- ladoB = 4;    
+
+Imprime "SIM" */ 
