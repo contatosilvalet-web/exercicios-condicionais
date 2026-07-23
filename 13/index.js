53934f47c260dd95e526bd6df997932e36d64281
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 14:24:34 -0300

    programa Compra com Desconto, verifica o tipo de desconto, calcule o valor do produto com o desconto e imprima o resultado

--- 13/index.js
+++ 13/index.js
@@ -1,6 +1,28 @@
-//tipo de pagamento (dinheiro, credito, debito, cheque).
 const tipoDePagamento = "credito";
-
-//valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if (tipoDePagamento === "credito") {
+    desconto = 0.05; 
+} else if (tipoDePagamento === "cheque") {
+    desconto = 0.03; 
+} else if (tipoDePagamento === "debito" || tipoDePagamento === "dinheiro") {
+    desconto = 0.10; 
+}
+
+const valorFinalEmCentavos = valorDoProduto - (valorDoProduto * desconto);
+
+const valorFinalReais = (valorFinalEmCentavos / 100).toFixed(2);
+
+console.log(`Valor a ser pago: R$${valorFinalReais}`);
+
+// Imprime "Valor a ser pago: R$123.50"
+
+/* Exemplos
+const tipoDePagamento = "dinheiro";
+const valorDoProduto = 13000;
+Imprime "Valor a ser pago: R$117.00"
+
+const tipoDePagamento = "cheque";
+const valorDoProduto = 13000;
+Imprime "Valor a ser pago: R$126.10"
+*/
