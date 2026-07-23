2124e734a6fb704f9852d1e34b5304f79b54986e
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 21:41:42 -0300

    Update index.js

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,39 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+if (idade < 12 || idade > 65 || possuiPatologia || altura < 150) {
+  console.log("ACESSO NEGADO");
+}
+
+else if (ehEstudante || idade < 18) {
+  console.log("10 reais");
+}
+
+else {
+  console.log("20 reais");
+}
+
+// Imprime "20 reais"
+
+/* Exemplos
+const idade = 10;
+const possuiPatologia = false;
+const altura = 110;
+const ehEstudante = false;
+
+Imprime "ACESSO NEGADO"
+
+const idade = 20;
+const possuiPatologia = false;
+const altura = 150;
+const ehEstudante = true;
+
+Imprime "10 reais"
+
+const idade = 17;
+const possuiPatologia = true;
+const altura = 160;
+const ehEstudante = true;
+
+Imprime "ACESSO NEGADO" */
+
