34c573a6c8fec4ec64fd187f6b7fbe92f9928d7c
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-22 21:37:31 -0300

    teste para pagamento ou isenção de imposto de renda
    Add tax exemption logic based on income and conditions.

--- 07/index.js
+++ 07/index.js
@@ -2,4 +2,30 @@ const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
-//seu código aqui
+if (aposentada || portadoraDeDoenca) {
+  console.log("ISENTA");
+}
+
+else if (totalDeRendimentos < 2284700) {
+  console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+}
+
+else if (totalDeRendimentos >= 2855970){
+  console.log("PEGA LEAO");
+}   
+
+// Imprime "PEGA LEAO"
+
+
+/* Exemplos
+const aposentada = true;
+const portadoraDeDoenca = false;
+const totalDeRendimentos = 3000000; 
+
+Imprime "ISENTA"
+
+const aposentada = false;
+const portadoraDeDoenca = false;
+const totalDeRendimentos = 2000000;
+
+Imprime "VAZA LEAO! JA TA DIFICIL SEM VOCE" */
